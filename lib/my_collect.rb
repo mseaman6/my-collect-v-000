9a00d40f26a6204e901110d9cb7c6788b60b210e
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-08 18:35:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def my_collect(empty_array)
+  i = 0
+  new_collection = []
+  while i < empty_array.length
+    new_collection << yield(empty_array[i])
+    i += 1
+  end
+  new_collection
+end
